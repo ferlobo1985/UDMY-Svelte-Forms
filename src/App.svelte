280fc1969b263ec6eb281bb6c6c132432062784e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-08 18:29:51 -0300

    02-Checkbox and Radio
    02-Checkbox and Radio

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
   let name = '';
   let age = '';
+  let terms = true;
+  let newsletter;
 
   $: console.log(name)
   $: console.log(age)
+  $: console.log(terms)
+  $: console.log(newsletter)
 </script>
 
 <div class="container">
@@ -19,5 +23,23 @@
     <input type="text" class="form-control" bind:value={age}>
   </div>
 
+  <hr/>
+
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" bind:checked={terms}>
+    <label for="terms" class="form-check-label">Terms</label>
+  </div>
+
+  <hr/>
+
+  <h3>Sub to newsletter</h3>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" name="newsletter" value="yes" bind:group={newsletter}> 
+    <label for="newsletter" class="form-check-label">YES</label>
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" name="newsletter" value="no" bind:group={newsletter}> 
+    <label for="newsletter" class="form-check-label">NO</label>
+  </div>
 
 </div>
