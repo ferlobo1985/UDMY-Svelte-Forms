02e27a423dea12037a5046c909719fae7b64157d
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-08 18:18:11 -0300

    init
    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let name = '';
+  let age = '';
+
+  $: console.log(name)
+  $: console.log(age)
+</script>
+
+<div class="container">
+  <h1>My form:</h1>
+
+  <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control" bind:value={name}>
+  </div>
+
+  <div class="mb-3">
+    <label for="age" class="form-label">Age</label>
+    <input type="text" class="form-control" bind:value={age}>
+  </div>
+
+
+</div>
